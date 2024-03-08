3d44b84d4bb59263aa8d6eb14265b173b2e4f406
Hardin <subhagyachhabra18718@gmail.com>
2024-03-08 16:44:43 +0530

    variables done

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 12345565
+let accountEmail = 'subhagya.chhabra@gmail.com'
+var accountPassword = '123456'
+accountCity = 'Sangrur'
+let accountState 
+// accountID = 45454 // not allowed 
+
+accountEmail = 'subhagya18718@gmail.com'
+accountPassword = '41414141'
+accountCity = 'Noida'
+/*
+    prefer not to use var , because of issue in block scope and functional scope
+*/
+console.log(accountID)
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
