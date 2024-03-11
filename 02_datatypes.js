05bd6b8b7fdcb682f2a0c35ea961372b55f07fbe
Hardin <subhagyachhabra18718@gmail.com>
2024-03-11 23:51:23 +0530

    conversions and opearations added

---
+++ 02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert('HELLO'); // we are using node js not browser
+console.log(3+3) 
+console.log('SUBHAGYA')
+// code readability should be high
+
+
+let name = 'subhagya'
+let age = 24
+let isLoggedIn = true
+
+// int 2 to the power 31
+//bigint 
+//string => ""
+// boolean => yes or no
+// NULL => standalone value
+// UNDEFINED => 
+// symbol => uniqueness
+
+//object => userdefined datatype
+
+console.log(typeof 'subhagya')
+console.log(typeof 90)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 03_conversion_operations.js
@@ -0,0 +1,51 @@
+let score = 'true'
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)
+
+// '33' = 33
+// '33abc' = NaN
+// true = 1 and false = 0
+
+let isLoggedIn = 'subhagya';
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1= true 
+// '' = false
+// 'fa' = true
+// 0 = false
+
+let number = 324;
+let stringNumber = String(number)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+
+/***************************************************operations*************************************************/
+let value = 3
+let negValue = -(value)
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**4);  // raise to the power 
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = ('hello')
+let str2 =(' subhagya');
+let str3 = str1+str2
+
+console.log(1+'2');
+console.log('1'+2);
+console.log('1'+2+2);
+console.log(2+2+'1');
+
+console.log((3+4) *5 %3 );
+
+console.log(+true);
+console.log(+'');
